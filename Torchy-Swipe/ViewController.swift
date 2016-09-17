6f9e4aa2e769b7a6167a08bb5d22f2747df2204c
Alex Knows <cruzbiz11@gmail.com>
2016-09-17 12:22:28 -0400

    Initial Commit

---
+++ Torchy-Swipe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Torchy-Swipe
+//
+//  Created by Alex Cruz on 9/17/16.
+//  Copyright © 2016 torchy-swipe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
